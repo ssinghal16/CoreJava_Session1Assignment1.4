7cbbb5f0ebc4d26e4caecdb15d6667ea1b751576
ssinghal16 <soniyasinghal1991@gmail.com>
2017-05-17 00:16:24 +0530

    Session 1 Assignment1.4

---
+++ Session1Assignment1.4/src/Assignment4.java
@@ -0,0 +1,14 @@
+//Write a program to print the characters corresponding to ASCII codes from 65 to 90.
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		char a; 
+		
+		for (int i = 65; i <=90; i++){
+			a = (char)i; 
+			System.out.println(i + "- " + a);
+		}
+
+	}
+
+}
